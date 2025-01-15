387761ec20968de7520e4d060d4b8cfcd7b6cbad
july-es-pino <julia.espinosa.garcia@gmail.com>
2025-01-15 14:05:44 -0600

    Desafio-Alura-01

---
+++ app.js
@@ -0,0 +1,23 @@
+alert ('Bienvenida y Bienvenido a nuestro sitio web');
+
+let nombre = ("Lua");
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 100;
+
+
+alert ("¡Error! completa todos los campos");
+
+let mensajeDeError = ('¡Error! completa todos los campos');
+alert (mensajeDeError);
+
+nombre = (prompt ("Ingresa tu Nombre"));
+console.log (nombre);
+
+edad =(prompt ('Ingresa tu edad'));
+console.log (edad);
+
+if (edad >= 17 ) {
+    alert ("¡Puedes obtener tu licencia de conducir!")
+    
+}
